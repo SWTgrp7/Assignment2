979a26e5d302d40d2096426017db264ae6c3c15d
Mathias Schjødt-Bavngaard <au618037@uni.au.dk>
2022-10-05 16:09:38 +0200

    added solution and test projekt

---
+++ Ladeskab/Ladeskab.Test.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ladeskab.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
