9258795457fc05433960deb5ae6bef0503de8f6c
gsglintborg <112693853+gsglintborg@users.noreply.github.com>
2022-10-12 15:47:17 +0200

    RFID reader

---
+++ Ladeskab/Ladeskab/Interfaces/IRFIDReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public class RFIDReaderEventArgs : EventArgs
+    {
+        public int RFID { get; set; }
+    }
+
+    public interface IRFIDReader
+    {
+        public event EventHandler<RFIDReaderEventArgs> RFIDEvent;
+        public void OnRfidRead(int rfid);
+    }
+}
---
+++ Ladeskab/Ladeskab/RFIDReader.cs
@@ -0,0 +1,19 @@
+﻿using Ladeskab.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public class RFIDReader : IRFIDReader
+    {
+        public event EventHandler<RFIDReaderEventArgs> RFIDEvent;
+
+        public void OnRfidRead(int rfid)
+        {
+            RFIDEvent?.Invoke(this, new RFIDReaderEventArgs() { RFID = rfid });
+        }
+    }
+}
